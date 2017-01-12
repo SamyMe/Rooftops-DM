b1d463c0b8354fafe24235ee72b65beff2754f88
Bennettson <billybill.bennettson@gmail.com>
2017-01-12 17:13:58 +0100

    Deletion of tons of useless files

--- demo_tsne/01_simplest_tsne.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import numpy as np
-from sklearn.manifold import TSNE
-
-
-X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-model = TSNE(n_components=2, random_state=0)
-np.set_printoptions(suppress=True)
-print(model.fit_transform(X))
